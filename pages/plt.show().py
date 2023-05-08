0ee5d82c03f62b0577a14e3c9301eb54477faa58
Max Bushuev <bushuevmaxim55@gmail.com>
2023-05-08 16:24:35 +0600

    plot and scatter

--- pages/plt.show().py
+++ pages/plt.show().py
@@ -3,42 +3,50 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.style.use('_mpl-gallery')
-
+data_mod = pd.read_csv("data/csgotask_m.csv")
 data = pd.read_csv("data/csgo_task.csv")
-plt.plot(data["ct_score"], data["t_score"])
-fig, ax = plt.subplots(figsize=(5, 5),)
-ax.plot(data["ct_score"], data["t_score"])
-plt.xlabel("ct_score")
-plt.ylabel("t_score")
-st.pyplot(fig)
 
-np.random.seed(3)
-x = data["ct_score"]
-y = data["t_score"]
-fig, ax = plt.subplots()
+data.dropna(inplace=True)
+if 'index' not in st.session_state:
+    st.session_state.index = 0
 
-ax.scatter(x, y, vmin=0, vmax=100)
+if 'features' not in st.session_state:
+    st.session_state.features = tuple(data_mod.columns)
+st.title("Зависимость признаков от целевого признака")
 
-plt.xlabel("ct_score")
-plt.ylabel("t_score")
+st.selectbox("Выберете признак для сравнения с целевым", options=st.session_state.features,
+             index=st.session_state.index, key='feature')
+st.session_state.index = st.session_state.features.index(
+    st.session_state.feature)
 
-st.pyplot(fig)
+plot, scatter = st.columns(2)
+x = data[st.session_state.feature]
+y = data["bomb_planted"]
+with plot:
+    plt.plot(data["ct_score"], data["t_score"])
+    fig, ax = plt.subplots(figsize=(5, 5),)
 
+    ax.plot(x, y)
+    plt.xlabel(st.session_state.feature)
+    plt.ylabel("bomb_planted")
+    st.pyplot(fig)
+with scatter:
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.scatter(x, y)
+    plt.xlabel(st.session_state.feature)
+    plt.ylabel("bomb_planted")
+    st.pyplot(fig)
+
+hist, pie = st.columns(2)
 x = 0.5 + np.arange(8)
 y = np.random.uniform(2, 7, len(x))
-
-fig, ax = plt.subplots()
-
+fig, ax = plt.subplots(figsize=(5, 5))
 ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
-
 st.pyplot(fig)
 
-x = data["map"].unique()
-colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7, len(x)))
-
-# plot
-fig, ax = plt.subplots()
-ax.pie(x, colors=colors, radius=3, center=(4, 4),labels=x,
+maps = dict((i, j) for i, j in enumerate(data["map"].unique()))
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.pie(maps.keys(), radius=3, center=(4, 4), labels=maps.values(),
        wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True)
+plt.legend()
 st.pyplot(fig)
