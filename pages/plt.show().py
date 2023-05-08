40ce964ff76828a4224589c4cf98910c61d350d7
Max Bushuev <bushuevmaxim55@gmail.com>
2023-05-08 17:27:03 +0600

    pie

--- pages/plt.show().py
+++ pages/plt.show().py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 data_mod = pd.read_csv("data/csgotask_m.csv")
 data = pd.read_csv("data/csgo_task.csv")
 data.dropna(inplace=True)
-print(data.columns)
+
 if 'index' not in st.session_state:
     st.session_state.index = 0
 
 if 'features' not in st.session_state:
     st.session_state.features = tuple(data.columns)
-st.title("Зависимость признаков от целевого признака")
+st.title("Визуализация данных")
 
 st.selectbox("Выберете признак для сравнения с целевым", options=st.session_state.features,
              index=st.session_state.index, key='feature')
@@ -37,16 +37,40 @@ with scatter:
     plt.ylabel("bomb_planted")
     st.pyplot(fig)
 
-hist, pie = st.columns(2)
-x = 0.5 + np.arange(8)
-y = np.random.uniform(2, 7, len(x))
-fig, ax = plt.subplots(figsize=(5, 5))
-ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
-st.pyplot(fig)
 
-maps = dict((i, j) for i, j in enumerate(data["map"].unique()))
+if 'maps' not in st.session_state:
+    st.session_state.maps = data["map"].unique()
+
+
+current_maps = st.multiselect("",
+                              st.session_state.maps, [], help="Выберете карту")
+
+
+hist, pie = st.columns(2)
+
+
+# maps = dict((i, mapa) for i, mapa in enumerate(data["map"].unique()) if mapa in )
+# maps = dict((mapa, data) for mapa in )
+maps = data.groupby(["map"])["map"].count()
+# maps_del = maps.drop(current_maps, axis=0)
+# print()
+# x = maps.to_frame() - maps_del.to_frame()
+# print(x)
+# y = data.groupby(["map", "bomb_planted"]).count()
+
+# plot
+# fig, ax = plt.subplots()
+
+# ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+# # x = data[current_maps]
+
+# fig, ax = plt.subplots(figsize=(5, 5))
+# ax.bar(maps, width=1, edgecolor="white", linewidth=0.7, height=1)
+# st.pyplot(fig)
+
+
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.pie(maps.keys(), radius=3, center=(4, 4), labels=maps.values(),
+ax.pie(maps, radius=3, center=(4, 4), labels=maps.keys(),
        wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True)
 plt.legend()
 st.pyplot(fig)
