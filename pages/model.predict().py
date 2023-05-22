a39d8580e28a5cdcfca868fff137c13cedbf8ae5
Max Bushuev <bushuevmaxim55@gmail.com>
2023-05-22 18:38:25 +0600

    f

--- pages/model.predict().py
+++ pages/model.predict().py
@@ -1,3 +1,5 @@
+import joblib
+import numpy as np
 import streamlit as st
 import pandas as pd
 st.title("Получение предсказания")
@@ -11,37 +13,35 @@ with left:
     ct_health = st.number_input("ct_health", min_value=0)
     ct_armor = st.number_input("ct_armor", min_value=0)
     ct_money = st.number_input("ct_money", min_value=0)
+    ct_helmets = st.number_input("ct_helmets", min_value=0)
     ct_players_alive = st.number_input("ct_players_alive", min_value=0)
+    time_left = st.number_input("time_left", min_value=0)
 with right:
     t_score = st.number_input("t_score", min_value=0)
     t_health = st.number_input("t_health", min_value=0)
     t_armor = st.number_input("t_armor", min_value=0)
     t_money = st.number_input("t_money", min_value=0)
+    t_helmets = st.number_input("t_helmets", min_value=0)
     t_players_alive = st.number_input("t_players_alive", min_value=0)
-time_left = st.number_input("time_left", min_value=0)
+    ct_defuse_kits = st.number_input("ct_defuse_kits", min_value=0)
+
+
 map = st.selectbox("Карта", data["map"].unique())
 button_click = st.button("Predict", )
-dataframe_test = pd.DataFrame(columns=data_new.columns)
-
+model = st.selectbox(
+    "Модель", ["Bagging Classifier", "Decision Tree", "Neural Network"])
 
 if button_click:
-    st.write(dataframe_test.columns)
-    row = [time_left, ct_score, t_score,
-           ct_health, t_health, ct_money, t_money,
-           ct_players_alive, t_players_alive,
+    row = np.array([[time_left, ct_score, t_score, map, ct_health, t_health, ct_armor, t_armor, ct_money, t_money,
 
-           ]
-    maps = ['map_de_cache', 'map_de_dust2', 'map_de_inferno',
-            'map_de_mirage', 'map_de_nuke', 'map_de_overpass', 'map_de_train',
-            'map_de_vertigo']
-    maplist = []
-    choosen_map = "map_"+map
-    for current_map in maps:
-        if current_map == choosen_map:
-            maplist.append(1)
-
-        else:
-            maplist.append(0)
-    row.extend(maplist)
-    dataframe_test.loc[len(dataframe_test.index)] = row
-    st.dataframe(dataframe_test)
+                    ct_helmets,	t_helmets,
+                    ct_defuse_kits,
+                    ct_players_alive, t_players_alive]])
+    dataframe_test = pd.DataFrame(
+        row, columns=data.drop(["bomb_planted"], axis=1).columns)
+    pipeline = joblib.load(f"models/{model}.joblib")
+    pred = np.around(pipeline.predict(dataframe_test))
+    if (pred == 1):
+        st.write("Бомба поставлена")
+    else:
+        st.write("Бомба не поставлена")
